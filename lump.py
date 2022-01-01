3bca09dee32da824b02536e645cd33b5ffdb2779
Ryan WS <wester5@clemson.edu>
2022-01-01 16:38:54 -0500

    Begin lump diagnostics script

---
+++ lump.py
@@ -0,0 +1,28 @@
+import numpy as np
+try:
+    import cupy as xp
+except ImportError:
+    import numpy as xp
+import importlib
+
+'''
+
+Lump diagnostics.
+
+'''
+
+def AsNumpy(x):
+    if xp.__name__ == 'cupy':
+        return xp.asnumpy(x)
+    elif xp.__name__ == 'numpy':
+        return x
+    else:
+        print("Error: xp must be cupy or numpy")
+
+def lump_weight(rho, xx, yy, r1, r2, dx):
+    theta = xp.arctan2(yy, xx)
+    wreal = np.trapz(np.trapz( AsNumpy(rho*xp.cos(theta)) dx=dx) dx=x)
+    wimag = np.trapz(np.trapz( AsNumpy(rho*xp.sin(theta)) dx=dx) dx=x)
+    wmag  = np.sqrt(wreal**2 + wimag**2)
+    wphase= np.arctan2(wimag, wreal)
+    return wmag, wphase
